e794b5583354f26a58b03ddd548ba60e8cd05266
whl <1023680139@qq.com>
2021-03-08 15:19:51 +0800

    feat: 将结构调整，使得界面仅需导入一个文件。

--- js/showBook/mainThree.js
+++ js/showBook/mainThree.js
@@ -1,4 +1,30 @@
+/**
+ * 这里是最基本的系统
+ * auther {whl}
+ */
 const app = {};
+app.Err = {};
+
+app.Err.errStr = `
+<h1>{{info}}</h1>
+`
+
+app.Err.error = function (info){
+
+    this.shower.innerHTML= this.errStr.replace('{{info}}', info);
+}
+
+app.Err.init = function (){
+
+    const child = document.createElement('div');
+    child.style.cssText = `
+        color:red;
+        background-color:black;
+    `;
+    document.body.appendChild(child);
+    this.shower = child;
+}
+
 
 /**
  * 初始化渲染器
@@ -75,6 +101,8 @@ app.initObject = function (){
  */
 app.init = function() {
 
+    this.Err.init();
+
     let info = this.initThree();
     if (info){
         Err.error(info);
@@ -106,6 +134,12 @@ app.init = function() {
     }
 
     this.renderCall = [];
+    this.objDefNum = 0;
+
+    app.T0 = new Date();
+    app.renderF();
+
+    return this;
 }
 
 /**
@@ -120,6 +154,7 @@ app.addAni = function({name = "NONAME", func, ...selfArea}) {
         isEnd :false,
         selfArea
     });
+    return this;
 }
 
 /**
@@ -133,9 +168,75 @@ app.delAni = function(name) {
         
         if (o.name === name){
             o.isEnd = true;
-            return;
+            return this;
         }
     }
 }
 
-app.init()
+/**
+ * 是否已经存在该物件
+ * @param {string} name 物件的名称
+ * @returns {boolean} 是否存在该物件
+ */
+app.hasObj = function(name){
+
+    return this.objs[name] !== undefined;
+}
+
+/**
+ * 向场景中添加物件
+ * @param {Three.Object} mesh 一个可以添加到场景中的物件
+ * @param {string} name 物件的名称，你可以使用名称从场景中获取物件
+ */
+app.addObj = function(mesh, name = '_objDefName' + (this.objDefNum++)){
+
+    if (this.hasObj(name)){
+        return this;
+    }
+    this.objs[name] = mesh;
+    this.scene.add(mesh);
+    return this;
+}
+
+/**
+ * 隐藏物件
+ * 其实现方式是通过将其从场景中移除
+ * @param {string} name 物件的名称
+ */
+app.hindObj = function (name){
+    this.scene.remove(this.objs[name]);
+    return this;
+}
+
+/**
+ * 显示物件
+ * 其实现方式是通过将其再次添加到场景中
+ * @param {string} name 物件的名称
+ */
+app.showObj = function (name){
+    this.scene.add(this.objs[name]);
+    return this;
+}
+
+/**
+ * 渲染部分
+ */
+app.renderF = function(){
+
+    let T1 = new Date();
+    let dt = T1 - app.T0; 
+    app.T0 = T1;
+
+    for (let i=app.renderCall.length-1; i >-1; i--){
+
+        let obj = app.renderCall[i];
+        let end = obj.func(dt, obj.selfArea);
+
+        if (end || obj.isEnd){
+            app.renderCall.splice(i, 1);
+        }
+    }
+
+    app.render.render(app.scene, app.camera);
+    requestAnimationFrame(app.renderF);
+}
